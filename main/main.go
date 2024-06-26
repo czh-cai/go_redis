bfd120d7a55e23463dad553fca0051b18488d9ed
czh <caizhaohua_2009@163.com>
2024-06-26 18:50:49 +0800

    go redis cluster

--- main/main.go
+++ main/main.go
@@ -4,26 +4,41 @@ import (
 	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	_ "github.com/redis/go-redis/v9"
+	"time"
 )
 
 func main() {
+	clusterNodes := []string{
+		"192.168.0.231:6380",
+		"192.168.0.231:6381",
+		"192.168.0.232:6382",
+		"192.168.0.232:6383",
+		"192.168.0.233:6384",
+		"192.168.0.233:6385",
+	}
+	// 创建一个 Redis Cluster 客户端
+	opts := &redis.ClusterOptions{
+		Addrs:    clusterNodes,
+		Password: "123123", // 如果你的 Redis 需要密码，请在这里设置
+	}
+
 	// 创建Redis客户端
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "192.168.0.231:6379", // Redis服务器地址
-		Password: "123456",             // 密码，没有则留空
-		DB:       0,                    // 使用默认DB
-	})
+	client := redis.NewClusterClient(opts)
 
 	ctx := context.Background()
 
 	// 设置键值
-	err := rdb.Set(ctx, "test_key", "test_value", 0).Err()
+	// specified duration is 1µs, but minimal supported value is 1ms - truncating to 1ms
+	//应该是duration 类型。这个时间单位不是s。
+	//Go语言文档中指出最小的时间间隔是1纳秒（1ns）,默认纳秒单位
+	err := client.Set(ctx, "test_key", "test_value", 5*60*time.Second).Err()
 	if err != nil {
 		panic(err)
 	}
 
 	// 获取键值
-	val, err := rdb.Get(ctx, "test_key").Result()
+	val, err := client.Get(ctx, "test_key").Result()
 	if err != nil {
 		panic(err)
 	}
@@ -31,7 +46,7 @@ func main() {
 	fmt.Println("key", val) // 输出: key value
 
 	// 关闭客户端连接
-	err = rdb.Close()
+	err = client.Close()
 	if err != nil {
 		panic(err)
 	}
